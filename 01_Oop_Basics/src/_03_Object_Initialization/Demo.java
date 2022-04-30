5802e00c22eae18ccd9dfa9f71141ea80347fc0d
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-30 22:31:28 +0530

    Object Initialization Exercise

--- 01_Oop_Basics/src/_03_Object_Initialization/Demo.java
+++ 01_Oop_Basics/src/_03_Object_Initialization/Demo.java
@@ -242,3 +242,163 @@ class Demo{
     }
 }
 */
+
+// -----------------------------------------------------------------------------------------
+
+// Exercise
+/*
+class Test{
+    int a=100;
+    int b;
+}
+class Demo{
+    public static void main(String args[]){
+        Test t1=new Test();
+        System.out.println("a : "+t1.a);
+        System.out.println("b : "+t1.b);
+    }
+}
+*/
+
+// Exercise
+/*
+class Test{
+    int a=100;
+    int b;
+    Test(){
+        a=50;
+        b=20;
+    }
+    Test(int i){
+
+    }
+    Test(int i, int j){
+        a=i;
+        b=j;
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        Test t1=new Test();
+        System.out.println("a : "+t1.a);
+        System.out.println("b : "+t1.b);
+
+        Test t2=new Test(1000);
+        System.out.println("a : "+t2.a);
+        System.out.println("b : "+t2.b);
+
+        Test t3=new Test(1,2);
+        System.out.println("a : "+t3.a);
+        System.out.println("b : "+t3.b);
+    }
+}
+*/
+
+// ---------------------------------------------------------------------------------------------
+
+//Case VI (Instance Blocks)
+//-------------------------
+/*
+class Test{
+    int a=100;
+    int b;
+    b=200; //Illegal
+	System.out.println("a statement...");//Illegal
+}
+class Demo{
+    public static void main(String args[]){
+        int a=100;
+        int b;
+        b=200; //Legal
+        System.out.println("a statement...");//Legal
+    }
+}
+*/
+
+
+// Using an instance block..
+//---------------------------
+/*
+class Test{
+    int a=100;
+    int b;
+    {
+        b=200;
+        System.out.println("Instance Block..");
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        Test t1=new Test();
+        System.out.println("a : "+t1.a);
+        System.out.println("b : "+t1.b); //200
+    }
+}
+*/
+
+// -------------------------------------------------------------------------------------------------
+
+//Exercise
+/*
+class Test{
+    int a=100;
+    int b=20;
+    {
+        b=200;
+        c=300;
+    }
+    int c=400;
+}
+class Demo{
+    public static void main(String args[]){
+        Test t1=new Test();
+        System.out.println("a : "+t1.a); //100
+        System.out.println("b : "+t1.b); //200
+        System.out.println("c : "+t1.c); //400
+    }
+}
+*/
+
+
+// Exercise
+// --------
+/*
+class Test{
+    int a=100;
+
+    {
+        b=200;
+    }
+    Test(){
+        c=1000;
+    }
+    int b=500;
+    Test(int i){
+
+    }
+    Test(int i, int j){
+        a=i;
+        b=j;
+    }
+    int c=400;
+}
+class Demo{
+    public static void main(String args[]){
+        Test t1=new Test();
+        System.out.println("a of t1  : "+t1.a);
+        System.out.println("b of t1  : "+t1.b);
+        System.out.println("c of t1  : "+t1.c);
+
+        Test t2=new Test(1);
+        System.out.println("a of t2  : "+t2.a);
+        System.out.println("b of t2  : "+t2.b);
+        System.out.println("c of t2  : "+t2.c);
+
+        Test t3=new Test(10,20);
+        System.out.println("a of t3  : "+t3.a);
+        System.out.println("b of t3  : "+t3.b);
+        System.out.println("c of t3  : "+t3.c);
+    }
+}
+*/
+
