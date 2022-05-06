0b9a5ce3385b69e33349a850b975133db9601036
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-06 08:18:27 +0530

    Class "List"

---
+++ 02_Encapsulation/src/_04_Class_List/Demo.java
@@ -0,0 +1,49 @@
+package _04_Class_List;
+
+// ========================================= Class List =========================================
+
+/*class Node{
+    int data;
+    Node next;
+    Node(int data){this.data=data;}
+}
+class Demo{
+    public static void main(String args[]){
+        List list=new List();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.printList(); //[10,20,30,40,50]
+
+        list.remove();
+        list.printList(); //[10,20,30,40]
+
+        list.remove(2); //public void remove(int index)
+        list.printList(); //[10,20,40]
+
+        list.add(2,300); //public void add(int index, int data)
+        list.printList(); //[10,20,300,40]
+
+        list.add(new int[]{1,2,3});//public void add(int[] dataArray);
+        list.printList(); //[10,20,300,40,1,2,3]
+
+        list.add(3,new int[]{400,500,600});//public void add(int index,int[] dataArray);
+        list.printList(); //[10,20,300,400,500,600,40,1,2,3]
+
+        list.remove(2,5); //public void remove(int startIndex, int endIndex);
+        list.printList(); //[10,20,40,1,2,3]
+
+        list.addFirst(11);
+        list.printList(); //[11,10,20,40,1,2,3]
+
+        list.addLast(99);
+        list.printList(); //[11,10,20,40,1,2,3,99]
+
+        list.removeFirst();
+        list.printList(); //[10,20,40,1,2,3,99]
+
+        list.removeLast();
+        list.printList(); //[10,20,40,1,2,3]
+    }
+}*/
