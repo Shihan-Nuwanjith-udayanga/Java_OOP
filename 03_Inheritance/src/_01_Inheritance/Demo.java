4cfc42a50945c24eed087594b532f8c1a77a10ba
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-07 08:21:48 +0530

    Java Inheritance Sample

---
+++ 03_Inheritance/src/_01_Inheritance/Demo.java
@@ -0,0 +1,33 @@
+package _01_Inheritance;
+
+// ============================== Java Inheritance ==========================================
+
+
+/*class Calculator{
+
+}
+class Demo{
+    public static void main(String args[]){
+        Calculator c1=new Calculator();
+        c1.setSize(300,300);
+        c1.setDefaultCloseOperation(3);
+        c1.setVisible(true);
+    }
+}*/
+
+/*import javax.swing.*;
+class Calculator extends JFrame{
+
+}
+class Demo{
+    public static void main(String args[]){
+        Calculator c1=new Calculator();
+        c1.setSize(300,300);
+        c1.setDefaultCloseOperation(3);
+        c1.setVisible(true);
+    }
+}*/
+
+// ===================================================================================================
+
+
