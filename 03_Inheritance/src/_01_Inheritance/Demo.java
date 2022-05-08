22bd2030ba9b3acb023b15faa4a80ca39ed19c74
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-08 08:04:44 +0530

    Java Inheritance Exercise

--- 03_Inheritance/src/_01_Inheritance/Demo.java
+++ 03_Inheritance/src/_01_Inheritance/Demo.java
@@ -595,3 +595,46 @@ class Demo{
     }
 }
 */
+
+//---------------------------------------------------------------------------------
+
+//Exercise
+
+/*
+class A{
+    void myMethod(){
+        System.out.println("myMethod() of A");
+    }
+}
+class B extends A{
+    void myMethod(){
+        System.out.println("myMethod() of B");
+    }
+}
+class C extends B {
+    void myMethod(){
+        System.out.println("myMethod() of C");
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        A a1=new A();
+        a1.myMethod();	//Line 1--> no dispatch
+
+        a1=new B();
+        a1.myMethod();	//Line 2-->dispatch
+
+        a1=new C();
+        a1.myMethod();	//Line 3-->dispatch
+
+        B b1=new B();
+        b1.myMethod();	//Line 4-->no
+
+        b1=new C();
+        b1.myMethod();	//Line 5-->yes
+
+        C c1=new C();
+        c1.myMethod();	//Line 6-->no
+    }
+}
+*/
