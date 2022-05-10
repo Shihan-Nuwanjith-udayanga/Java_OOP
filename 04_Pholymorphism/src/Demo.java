45f464c79ad6c91367bf205c23d0003c8d9f1bcf
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-10 09:05:10 +0530

    Runtime polymorphism

--- 04_Pholymorphism/src/Demo.java
+++ 04_Pholymorphism/src/Demo.java
@@ -18,3 +18,72 @@ class Demo{
     }
 }
 */
+
+// -------------------------------------------------------------------------------------
+
+//Runtime Polymorphism
+//--------------------
+/*
+class Vehicle{
+    public void park(int location){
+        System.out.println("Vehicle Parking... "+location);
+    }
+}
+class Car extends Vehicle{}
+class Van extends Vehicle{}
+class Bus extends Vehicle{}
+
+class Demo{
+    public static void main(String args[]){
+        Vehicle v1;
+        v1=new Car();
+        v1.park(1001); //Car Parking...1001
+
+        v1=new Van();
+        v1.park(2002); //Van Parking...2002
+
+        v1=new Bus();
+        v1.park(3003); //Bus Parking...3003
+    }
+}
+*/
+
+
+/*
+class Vehicle{
+    public void park(int location){
+        System.out.println("Vehicle Parking... "+location);
+    }
+}
+class Car extends Vehicle{
+    public void park(int location){
+        System.out.println("Car Parking... "+location);
+    }
+}
+class Van extends Vehicle{
+    public void park(int location){
+        System.out.println("Van Parking... "+location);
+    }
+}
+class Bus extends Vehicle{
+    public void park(int location){
+        System.out.println("Bus Parking... "+location);
+    }
+}
+
+class Demo{
+    public static void main(String args[]){
+        Vehicle v1;
+        v1=new Car();
+        v1.park(1001); //Car Parking...1001
+
+        v1=new Van();
+        v1.park(2002); //Van Parking...2002
+
+        v1=new Bus();
+        v1.park(3003); //Bus Parking...3003
+    }
+}
+*/
+
+// -------------------------------------------------------------------------------------------
