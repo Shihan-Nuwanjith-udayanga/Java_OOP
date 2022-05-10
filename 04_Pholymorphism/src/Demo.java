50cdc2e98c551469127eb2610afdf0ffdb1c94f0
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-10 09:03:31 +0530

    Compiletime polymorphism

---
+++ 04_Pholymorphism/src/Demo.java
@@ -0,0 +1,20 @@
+
+/////////////////////////////////// Polymorphism  //////////////////////////////////
+
+//Compiletime polymorphism
+//------------------------
+/*
+class Demo{
+    public static void main(String args[]){
+        int x=-12;
+        long y=-23;
+        float f=-23.23f;
+        double d=2323.23232;
+
+        x=Math.abs(x); //single interface-->Math.abs(args)
+        y=Math.abs(y);
+        f=Math.abs(f);
+        d=Math.abs(d);
+    }
+}
+*/
