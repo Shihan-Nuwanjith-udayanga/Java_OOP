54ef53bb9146c5cdad3b85ab3fd596e700379d7d
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-13 08:37:17 +0530

    Class "Object"

---
+++ 05_Class_Object/src/Demo.java
@@ -0,0 +1,45 @@
+
+// ======================== Class "Object"	=====================================
+
+/*
+class Customer{ //compiler inserts "extends Object"
+
+}
+class Demo{
+    public static void main(String args[]){
+        Customer c1=new Customer();
+        c1.toString(); //Legal
+        c1.hashCode();
+        c1.equals(c1);
+        c1.getClass();
+        c1.myMethod(); //Illegal
+    }
+}
+*/
+
+/*
+class Customer{ //compiler inserts "extends java.lang.Object"
+    Customer(){
+        super(); //Legal
+    }
+}
+*/
+
+
+/*
+import javax.swing.*;
+class Customer{ }
+
+class Demo{
+    public static void main(String args[]){
+        Object ob;
+        ob=new Customer();
+        ob=new String();
+        ob=new JFrame();
+        ob=new JButton();
+        ob=new int[10];
+
+    }
+}
+*/
+
