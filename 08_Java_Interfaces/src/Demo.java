ae794ba6c5e072f65e95cbecde7988fc8cf00fb5
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-23 06:53:52 +0530

    Java Interface Example

---
+++ 08_Java_Interfaces/src/Demo.java
@@ -0,0 +1,17 @@
+
+// ===================================== Java Interfaces ==============================================
+
+/*abstract class Vehicle{
+	abstract public void park();
+	abstract public void start();
+	abstract public void stop();
+}*/
+/*
+
+interface Vehicle{
+    public void park(); //implicitly public, abstract
+    public void start();
+    public void stop();
+}
+*/
+
