d4ae75e5a7fcfb7b5089e574b7c3e05929c459cf
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-23 06:55:32 +0530

    Java Interface Cases

--- 08_Java_Interfaces/src/Demo.java
+++ 08_Java_Interfaces/src/Demo.java
@@ -15,3 +15,50 @@ interface Vehicle{
 }
 */
 
+// -------------------------------------------------------------------------------------------------
+
+//Case I
+//------
+/*
+interface Vehicle{
+
+}
+//class Car extends Vehicle{} //Illegal
+class Car implements Vehicle{} //Legal
+*/
+
+
+//Case II
+//-------
+/*
+interface Vehicle{
+    public void park();
+    public void start();
+}
+class Car implements Vehicle{} //Illegal
+class Van implements Vehicle{ //Illegal
+    public void park(){
+
+    }
+}
+class Bus implements Vehicle{ //legal
+    public void park(){
+
+    }
+    public void start(){
+
+    }
+}
+*/
+
+
+//Case III
+//--------
+/*
+interface Vehicle{
+    void park(); //implicitly public
+    public void start();
+    protected void stop(); //Illegal
+
+}
+*/
