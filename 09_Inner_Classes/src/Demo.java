28f46c82c42c662d6d5f87a3360a56bee53e7b0e
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-27 08:30:11 +0530

    Inner Classes, Event Handling

---
+++ 09_Inner_Classes/src/Demo.java
@@ -0,0 +1,61 @@
+
+// ==================================== Inner Classes, Event Handling ===========================================
+
+//Q01
+/*
+import javax.swing.*;
+import java.awt.*;
+class MyFrame extends JFrame{
+    JButton exitButton;
+    MyFrame(){
+        setTitle("MyFrame");
+        setSize(300,300);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        exitButton=new JButton("Exit");
+        exitButton.setFont(new Font("",1,25));
+        add(exitButton);
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        new MyFrame().setVisible(true);
+    }
+}
+*/
+
+//Q02
+/*
+import javax.swing.*;
+import java.awt.*;
+class MyFrame extends JFrame{
+    JButton exitButton;
+    MyFrame(){
+        setTitle("MyFrame");
+        setSize(300,300);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        exitButton=new JButton("Exit");
+        exitButton.setFont(new Font("",1,25));
+        A a1=new A();
+        exitButton.addActionListener(a1); //Illegal
+        add(exitButton);
+    }
+}
+class A{
+    public void m(){
+        System.exit(0);
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        new MyFrame().setVisible(true);
+    }
+}
+*/
+
+
