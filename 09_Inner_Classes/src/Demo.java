df29e1122b737ead196f0c11ae30c676ec26f9b2
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-28 10:47:41 +0530

    (ActionListener vs ActionEvent)

--- 09_Inner_Classes/src/Demo.java
+++ 09_Inner_Classes/src/Demo.java
@@ -58,4 +58,38 @@ class Demo{
 }
 */
 
+//Q03 From Q02 (ActionListener vs ActionEvent)
+//--------------------------------------------
+/*
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class MyFrame extends JFrame{
+    JButton exitButton;
+    MyFrame(){
+        setTitle("MyFrame");
+        setSize(300,300);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        exitButton=new JButton("Exit");
+        exitButton.setFont(new Font("",1,25));
+        exitButton.addActionListener(new A());
+        add(exitButton);
+    }
+}
+class A implements ActionListener{
+    public void actionPerformed(ActionEvent evt){
+        System.exit(0);
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        new MyFrame().setVisible(true);
+    }
+}
+*/
+
+
 
