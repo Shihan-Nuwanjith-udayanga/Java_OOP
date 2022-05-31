fe3940ce0100fb1fc3e2b838ed245ee49312785e
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-05-31 11:20:49 +0530

    Lambda Expression

---
+++ 10_Lambda_Expression/src/Demo.java
@@ -0,0 +1,26 @@
+
+// ====================================== Lambda Expression =================================================
+
+/*
+interface  Vehicle{
+    public void park();
+}
+
+class Demo{
+    public static void main(String args[]){
+		*/
+/*Vehicle v1=new Vehicle(){
+			public void park(){
+				System.out.println("Car Parking..");
+			}
+		};
+		v1.park();
+		*//*
+
+        Vehicle v1=	()->{
+            System.out.println("Car Parking..");
+        };
+        v1.park();
+    }
+}
+*/
