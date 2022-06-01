2b718e33414a7237af7e11cc90c5959561b27e51
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-06-01 08:38:05 +0530

    Exercise (Lambda Expression)

--- 10_Lambda_Expression/src/Demo.java
+++ 10_Lambda_Expression/src/Demo.java
@@ -294,7 +294,27 @@ class Demo{
 */
 
 
+//Q04 (Lambda Expression)
+//-----------------------
+/*
+interface  Arithmetic{
+    public int  operate(int num1, int num2);
+}
+class Demo{
+    public static void main(String args[]){
+        Arithmetic add=(num1,num2)->num1+num2;
+        Arithmetic sub=(num1,num2)->num1-num2;
+        Arithmetic mul=(num1,num2)->num1*num2;
+        Arithmetic div=(num1,num2)->num1/num2;
 
+        int x=100,y=50;
+        System.out.println(x+" + "+y+" = "+add.operate(x,y));
+        System.out.println(x+" - "+y+" = "+sub.operate(x,y));
+        System.out.println(x+" * "+y+" = "+mul.operate(x,y));
+        System.out.println(x+" / "+y+" = "+div.operate(x,y));
+    }
+}
+*/
 
 
 
