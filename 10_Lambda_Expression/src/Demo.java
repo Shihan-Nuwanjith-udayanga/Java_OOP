7943c429fd9fd38d68e866d9b9adec42eeb72f0e
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-06-01 08:36:11 +0530

    Exercise (Local Inner)

--- 10_Lambda_Expression/src/Demo.java
+++ 10_Lambda_Expression/src/Demo.java
@@ -212,6 +212,47 @@ class Demo{
 }
 */
 
+//Q02 From Q01 (Local Inner)
+//--------------------------
+/*
+interface  Arithmetic{
+    public int  operate(int num1, int num2);
+}
+class Demo{
+    public static void main(String args[]){
+        class Addition implements Arithmetic{
+            public int  operate(int num1, int num2){
+                return num1+num2;
+            }
+        }
+        class Substraction implements Arithmetic{
+            public int  operate(int num1, int num2){
+                return num1-num2;
+            }
+        }
+        class Multiplication implements Arithmetic{
+            public int  operate(int num1, int num2){
+                return num1*num2;
+            }
+        }
+        class Division implements Arithmetic{
+            public int  operate(int num1, int num2){
+                return num1/num2;
+            }
+        }
+        Arithmetic add=new Addition();
+        Arithmetic sub=new Substraction();
+        Arithmetic mul=new Multiplication();
+        Arithmetic div=new Division();
+
+        int x=100,y=50;
+        System.out.println(x+" + "+y+" = "+add.operate(x,y));
+        System.out.println(x+" - "+y+" = "+sub.operate(x,y));
+        System.out.println(x+" * "+y+" = "+mul.operate(x,y));
+        System.out.println(x+" / "+y+" = "+div.operate(x,y));
+    }
+}
+*/
 
 
 
