3118f9cb314ff9da15fa61f9b8d0a5b705b9423a
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-06-06 07:35:32 +0530

    Garbage Collections Cases

--- 12_Garbage_Collections/src/Demo.java
+++ 12_Garbage_Collections/src/Demo.java
@@ -38,6 +38,75 @@ class Demo{
 */
 
 
+//Case I
+//------
+/*
+class MyClass{
+    long[] xr=new long[100000];
+    public void finalize(){
+        System.out.println("deleted...");
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        System.out.println("start");
+        MyClass[] cr=new MyClass[10000];
+        for (int i = 0; i < 10000; i++){
+            System.out.println("i : "+i);
+            new MyClass();
+        }
+        System.out.println("end");
+    }
+}
+*/
+
+
+//Case II
+//-------
+/*
+class MyClass{
+    long[] xr=new long[100000];
+    public void finalize(){
+        System.out.println("deleted...");
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        System.out.println("start");
+        new MyClass();
+        new MyClass();
+        new MyClass();
+        try{Thread.sleep(1000);}catch(Exception ex){}
+        System.out.println("end");
+    }
+}
+*/
+
+
+//Case III
+//--------
+/*
+class MyClass{
+    long[] xr=new long[100000];
+    public void finalize(){
+        System.out.println("deleted...");
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        System.out.println("start");
+        new MyClass();
+        new MyClass();
+        new MyClass();
+        System.gc(); //request-->Runtime.getRuntime().gc();
+        try{Thread.sleep(1000);}catch(Exception ex){}
+        System.out.println("end");
+    }
+}
+*/
+
+
+
 
 
 
