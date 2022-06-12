eb2a96cc62183525530b3d785098959fc6fbde86
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-06-12 07:08:02 +0530

    Enumerations (enums)

--- 13_Enumerations/src/Demo.java
+++ 13_Enumerations/src/Demo.java
@@ -32,3 +32,74 @@ class Demo{
     }
 }
 */
+
+
+//Q03
+/*
+enum Colour{
+    RED, GREEN, BLACK, BLUE; //enum Objects
+}
+class Demo{
+    public static void main(String args[]){
+        Colour c1=Colour.RED;
+        switch(c1){
+            case BLACK :
+            case RED : //Legal
+        }
+    }
+}
+//byte, short, int, char, String, enums
+*/
+
+
+//Q04
+/*
+enum Colour{
+    RED(), GREEN(), BLACK(), BLUE; //BLUE-->BLUE()
+    Colour(){
+        System.out.println("Colour()");
+    }
+}
+class Demo{
+    public static void main(String args[]){
+        Colour c1=Colour.RED;
+    }
+}
+*/
+
+
+//Q05
+/*
+enum Colour{
+    RED(1001), GREEN(1002), BLACK(), BLUE(1003);
+    private int code;
+    Colour(){
+        System.out.println("Colour()");
+    }
+    Colour(int code){
+        System.out.println("Colour(int)");
+        this.code=code;
+    }
+    public void setCode(int code){this.code=code;}
+    public int getCode(){return code;}
+}
+class Demo{
+    public static void main(String args[]){
+        Colour c1=Colour.RED;
+        System.out.println("code RED : "+c1.getCode());
+        c1.setCode(2222);
+        System.out.println("code RED : "+c1.getCode());
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
