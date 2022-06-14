35ec31c600a72d55f5ce88cc3040074da2806d40
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-06-14 09:18:06 +0530

    Variable length arguments (Var-args) Since JDK1.5

---
+++ 14_Variable_length_arguments/src/Demo.java
@@ -0,0 +1,23 @@
+
+// ============================= Variable length arguments (Var-args) Since JDK1.5 =============================
+
+/*
+class Demo{
+    public static void myMethod(int x){
+        System.out.println("myMethod(int)");
+    }
+    public static void myMethod(int x, int y){
+        System.out.println("myMethod(int,int)");
+    }
+    public static void myMethod(int x, int y, int z){
+        System.out.println("myMethod(int,int,int)");
+    }
+    public static void main(String args[]){
+        Demo.myMethod(100);
+        Demo.myMethod(100,200);
+        Demo.myMethod(100,200,300);
+        //Demo.myMethod(100,200,300,400);
+    }
+}
+*/
+
