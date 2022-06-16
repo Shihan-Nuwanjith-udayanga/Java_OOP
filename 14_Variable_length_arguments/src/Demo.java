bc24700426206e7981fb90cceb633ce16728b0e2
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-06-16 06:08:22 +0530

    Variable length arguments

--- 14_Variable_length_arguments/src/Demo.java
+++ 14_Variable_length_arguments/src/Demo.java
@@ -21,3 +21,26 @@ class Demo{
 }
 */
 
+/*
+class Demo{
+    public static void myMethod(int...x){
+        System.out.println("myMethod(int)");
+    }
+    */
+/*public static void myMethod(int x, int y){
+        System.out.println("myMethod(int,int)");
+    }
+    public static void myMethod(int x, int y, int z){
+        System.out.println("myMethod(int,int,int)");
+    }*//*
+
+    public static void main(String args[]){
+        Demo.myMethod(100);
+        Demo.myMethod(100,200);
+        Demo.myMethod(100,200,300);
+        Demo.myMethod(100,200,300,400);
+    }
+}
+*/
+
+
